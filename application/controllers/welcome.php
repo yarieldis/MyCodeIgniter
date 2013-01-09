cc1ccf0881e7f549f3440f78f43064b4487c05f8
Yarieldis Claro Soto <yarieldis@gmail.com>
2013-01-09 16:53:43 -0500

    adding composer and etecsa sqlite code

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -16,6 +16,29 @@ class Welcome extends CI_Controller {
 		
 		$this->twig->view('show.html.twig');
 	}
+
+	public function sqlite() {
+		try {
+			$dbh = new PDO("sqlite:H:\\etecsa.sqlite");
+			
+			$res = $dbh->query("SELECT COUNT(*) FROM guia WHERE type='Prepago' AND address LIKE '%VEDADO%'");
+			$row = $res->fetch(PDO::FETCH_NUM);
+			print_r($row);
+			exit;
+			
+			foreach ($result as $row) {
+				$phone->name = $row['name'];
+				$phone->phone = $row['telephone'];
+				$phone->ci = $row['ci'];
+				$phone->address = $row['address'];
+				$phone->province = $row['province'];
+			}
+			echo $result_count == $page_count ? ($page+$result_count) : 'end';
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
+	
 }
 
 /* End of file welcome.php */
